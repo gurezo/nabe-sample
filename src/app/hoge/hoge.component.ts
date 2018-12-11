db332f2e03e98e34f74570f0b2d7c02a103b136d
gurezo <akihiko.kigure@gmail.com>
2018-12-11 22:06:02 +0900

    実装変更

--- src/app/hoge/hoge.component.ts
+++ src/app/hoge/hoge.component.ts
@@ -6,20 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hoge.component.scss']
 })
 export class HogeComponent implements OnInit {
-
   // ボタンのループ処理用JSON
-  data = [{ title: 'Alpha', index: 1 }, { title: 'Beta', index: 2 }, { title: 'Ganma', index: 3 }];
+  data = [
+    { title: 'Alpha', index: 1 },
+    { title: 'Beta', index: 2 },
+    { title: 'Ganma', index: 3 }
+  ];
 
   constructor() {}
 
   ngOnInit() {}
 
-  onClicK(idx) {
-    // alert('インデックス: ' + idx);
-    if (idx === 2) {
+  // onClicK(idx) {
+  //   // alert('インデックス: ' + idx);
+  //   if (idx === 2) {
+  //     alert('Yahooo!!!!');
+  //   } else {
+  //     alert('インデックス: ' + idx);
+  //   }
+  // }
+
+  // 引数を{ title: '〜〜〜', index: ？ }で受け取り
+  onClicK(rec) {
+    if (rec.title === 'Beta') {
       alert('Yahooo!!!!');
     } else {
-      alert('インデックス: ' + idx);
+      alert('インデックス: ' + recidx);
     }
   }
 }
