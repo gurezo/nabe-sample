a5222bb10ca881947a9ac1159b36e6653c07943a
gurezo <akihiko.kigure@gmail.com>
2018-12-11 22:34:52 +0900

    スピナー実装

--- src/app/bar/bar.module.ts
+++ src/app/bar/bar.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { BarComponent } from './bar.component';
 import { BarRoutingModule } from './bar-routing.module';
 
 @NgModule({
   declarations: [BarComponent],
-  imports: [CommonModule, MatProgressBarModule, SharedModule, BarRoutingModule]
+  imports: [
+    CommonModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    SharedModule,
+    BarRoutingModule
+  ]
 })
 export class BarModule {}
