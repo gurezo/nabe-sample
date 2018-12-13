2b618c1596f23faaee0904b8d8f997f5b09f4041
gurezo <akihiko.kigure@gmail.com>
2018-12-13 23:24:53 +0900

    コード追加・解説追加

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 declare function require(x: any): any; // requireを使うための宣言
 
+// 仕事の実装では、serviceに書くコード
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  url = './assets/json/data.json';  // サーバー取得する場合のurlの定義
 
-  // データの配列 table.component.html L11~
+  // データの配列 table.component.html L22~
   recJson = require('../../assets/json/data.json'); // requireと言う方法でインポート
   recArray = [];
 
-  // 見慣れた実装
+  // 見慣れた実装(JSON直書き)
   headrData = [
     {
       title: 'ID',
@@ -29,12 +33,31 @@ export class TableComponent implements OnInit {
     }
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     // ループ用にデータ取り出し
     this.recArray = this.recJson.data;
     console.log(this.recArray);
     console.log(this.headrData);
+
+    // 仕事の実装では、serviceに書くコード
+    // データの配列 table.component.html L22~
+    this.getJsonData().subscribe(value => { // => {} と言う書き方がアロー関数です。
+      console.log(value);
+      // データ取り出し用関数を呼び出し => このファイル内のL58へ
+      this.recArray = this.getData(value);
+      console.log(this.recArray);
+    });
+  }
+
+  // 仕事の実装では、serviceに書くコード
+  getJsonData() {
+    return this.http.get(this.url);
+  }
+
+  // データ取り出し用関数
+  getData(value) {
+    return value.data;
   }
 }
--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { TableRoutingModule } from './table-routing.module';
-import { DataTablesModule } from 'angular-datatables';
 
 import { TableComponent } from './table.component';
 
+// 仕事の実装では、serviceに書くコード
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, DataTablesModule, SharedModule, TableRoutingModule]
+  imports: [
+    CommonModule,
+    HttpClientModule, // 仕事の実装では、serviceに書くコード
+    SharedModule,
+    TableRoutingModule
+  ]
 })
 export class TableModule {}
